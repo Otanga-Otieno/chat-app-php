0ada27dfbd9aed479dc0e22cf5de725958f9c422
Otanga-Otieno <otieno.otanga@gmail.com>
2021-11-01 16:53:20 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+require "header.htm";
+
+?>
+
+
+<div class="container" style="text-align: center;">
+
+    <form class="p-3" action="">
+        <input class="rounded" type="text" name="search" placeholder="Search global users">
+    </form>
+
+</div>
