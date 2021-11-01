e2bcac8d2b5c643c44f8d6599e85865439cd6e44
Otanga-Otieno <otieno.otanga@gmail.com>
2021-11-01 14:30:19 +0300

    Fix typo

--- forms.php
+++ forms.php
@@ -2,7 +2,7 @@
 
 require "functions.php";
 
-if(isset $_POST('signup')) {
+if(isset($_POST['signup']) {
 
     $email = $_POST['uemail'];
     $passwordHash = password_hash($_POST['upassword']);
@@ -11,7 +11,7 @@ if(isset $_POST('signup')) {
 
 }
 
-if(isset $_POST('signin')) {
+if(isset($_POST['signin']) {
 
     $email = $_POST['uemail'];
     $passwordHash = password_hash($_POST['upassword']);
