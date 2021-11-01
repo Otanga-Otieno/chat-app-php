c3083badf28c3216402c4e1d549f97c0e4fe4ec6
Otanga-Otieno <otieno.otanga@gmail.com>
2021-11-01 15:00:40 +0300

    Add user authentication functions

--- functions.php
+++ functions.php
@@ -5,3 +5,62 @@ require "config.php";
 session_start();
 
 $conn = new mysqli(Config::SERVER_NAME, Config::USER_NAME, Config::PASSWORD, Config::DB_NAME);
+
+
+function create_user($email, $passwordHash, $active = 1) {
+
+    global $conn;
+
+    $stmt = $conn->prepare("INSERT INTO users(uemail, passwordHash, uactive) VALUES(?,?,?)");
+    $stmt->bind_param("ssi", $email, $passwordHash, $active);
+    $stmt->execute();
+    $stmt->close();
+
+}
+
+function check_user($user) {
+
+    global $conn;
+
+    $stmt = $conn->prepare("SELECT id FROM users WHERE uemail = ?");
+    $stmt->bind_param("s", $user);
+    $stmt->execute();
+    $stmt->bind_result($result);
+    $stmt->fetch();
+    $stmt->close();
+    return isset($result) ? true : false;
+
+}
+
+function check_active($user) {
+
+    global $conn;
+
+    $stmt = $conn->prepare("SELECT uactive FROM users WHERE uemail = ?");
+    $stmt->bind_param("s", $user);
+    $stmt->execute();
+    $stmt->bind_result($result);
+    $stmt->fetch();
+    $stmt->close();
+    return $result == 1 ? true : false;
+
+}
+
+
+function check_active_user($user) {
+
+    return check_user($user) && check_active($user) ? true : false;
+
+}
+
+function login($user) {
+
+    $_SESSION['user'] = $user;
+
+}
+
+function logout() {
+
+    unset($_SESSION['user']);
+
+}
