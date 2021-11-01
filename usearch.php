fbbd4b15ce69bd7bba09580eea4b2bff363f9c48
Otanga-Otieno <otieno.otanga@gmail.com>
2021-11-01 22:14:01 +0300

    Implement search users function

--- usearch.php
+++ usearch.php
@@ -6,12 +6,4 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
     $name = $_POST['name'];
     $data = search_users($name);
     echo json_encode($data);
-
-    /*if($name == "") {
-        $data = all_users();
-        echo json_encode($data);
-    } else {
-        $data = search_users($name);
-        echo json_encode(array_values($data));
-    }*/    
 }
