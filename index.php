b8b6d7d15f97b6c280ddc68559946ac81b2bf1de
Otanga-Otieno <otieno.otanga@gmail.com>
2021-11-02 19:33:41 +0300

    Style user list

--- index.php
+++ index.php
@@ -10,14 +10,16 @@ $all = all_users();
 
 <div class="container" style="text-align: center;">
 
-    <form class="pt-3 mb-0" action="">
-        <input class="rounded" type="text" name="search" placeholder="Search global users" oninput="searchUser(this.value)" autocomplete="off">
-    </form>
-
-    <ul class="mt-0" id="userList" style="list-style-type: none; padding: 0; background-color: black; color: white;">
-    <?php foreach($all as $email) { ?>
-        <li><?php echo $email; ?></li><hr>
-    <?php } ?>
-    </ul>
+    <div>
+        <form class="pt-3 mb-0" action="">
+            <input class="rounded" style="width: 100%;" type="text" name="search" placeholder="Search global users" oninput="searchUser(this.value)" autocomplete="off">
+        </form>
+    
+        <ul class="mt-0" id="userList" style="list-style-type: none; padding: 0; background-color: black; color: white; width: 100%;">
+        <?php foreach($all as $email) { ?>
+            <li><?php echo $email; ?></li><hr>
+        <?php } ?>
+        </ul>
+    </div>
 
 </div>
