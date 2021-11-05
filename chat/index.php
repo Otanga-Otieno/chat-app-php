cbe424e999a80c8e763cd84970a7ff19a7f21846
Otanga-Otieno <otieno.otanga@gmail.com>
2021-11-05 16:54:56 +0300

    Style chat page

--- chat/index.php
+++ chat/index.php
@@ -14,7 +14,7 @@ $to = $_GET['to'];
         <div style="background-color: #77d7c8; text-align: center; width: 100%;">
             <span><h3 style="color: white;"><?php echo $to; ?></h3></span>
         </div>
-        <div style="background-color: white; height: 70vh;"></div>
+        <div style="background-color: white; height: 70vh; width: 100%;"></div>
         <div style="background-color: #77d7c8; width: 100%;">
             <form action="">
                 <input class="rounded" type="text" placeholder="Type a message" style="width: 100%; color: black;">
