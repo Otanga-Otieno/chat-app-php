2c5cf55df32f57c15eb2bc301393c36fa9ea322f
Otanga-Otieno <otieno.otanga@gmail.com>
2021-11-05 12:25:49 +0300

    Style chat page

--- chat/index.php
+++ chat/index.php
@@ -10,11 +10,11 @@ $to = $_GET['to'];
 
 <div class="container">
     
-    <div class="rounded">
-        <div style="background-color: #77d7c8;">
+    <div class="p-5 rounded">
+        <div style="background-color: #77d7c8; text-align: center;">
             <span><h3 style="color: white;"><?php echo $to; ?></h3></span>
         </div>
-        <div style="background-color: white; height: 80%;"></div>
+        <div style="background-color: white; height: 70vh;"></div>
         <div style="background-color: #77d7c8;">
             <form action="">
                 <input class="rounded" type="text" placeholder="Type a message" style="width: 100%; color: black;">
