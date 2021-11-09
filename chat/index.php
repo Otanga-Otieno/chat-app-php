78f2401c235a1f977fb81acb713a265ce26a925b
Otanga-Otieno <otieno.otanga@gmail.com>
2021-11-09 10:29:38 +0300

    Call onload from body

--- chat/index.php
+++ chat/index.php
@@ -11,23 +11,27 @@ $sender = $_SESSION['user'];
 
 <script ></script>
 
-<div class="container" onload='livereceiver("<?php echo $sender; ?>", "<?php echo $receiver; ?>");'>
+<body onload='livereceiver("<?php echo $sender; ?>", "<?php echo $receiver; ?>");'>
     
-    <div class="m-5 rounded" style="border: white solid 1px;">
-        <div style="background-color: #77d7c8; text-align: center; width: 100%;">
-            <span><h3 style="color: white;"><?php echo $receiver; ?></h3></span>
+    <div class="container">
+    
+        <div class="m-5 rounded" style="border: white solid 1px;">
+            <div style="background-color: #77d7c8; text-align: center; width: 100%;">
+                <span><h3 style="color: white;"><?php echo $receiver; ?></h3></span>
+            </div>
+            <div class="p-2" id="chatbox" style="background-color: white; height: 70vh; width: 100%; overflow-y: scroll;">
+                <?php get_chats($sender, $receiver); ?>
+            </div>
+            <div style="background-color: #77d7c8; width: 100%;">
+                <form action="">
+                    <input class="rounded" id="msg" type="text" placeholder="Type a message" style="width: 75%; color: black; display: inline-block;">
+                    <input id="rec" type="text" name="receiver" value="<?php echo $receiver; ?>" hidden>
+                    <input id="sen" type="text" name="sender" value="<?php echo $_SESSION['user']; ?>" hidden>
+                    <span name="sendMessage" onclick="sendText(); return false;" style="width: 20%; display: inline-block; background-color: transparent; border: none;"><i class="fas fa-paper-plane" style="display: inline;"></i></span>
+                </form>
+            </div>
         </div>
-        <div class="p-2" id="chatbox" style="background-color: white; height: 70vh; width: 100%; overflow-y: scroll;">
-            <?php get_chats($sender, $receiver); ?>
-        </div>
-        <div style="background-color: #77d7c8; width: 100%;">
-            <form action="">
-                <input class="rounded" id="msg" type="text" placeholder="Type a message" style="width: 75%; color: black; display: inline-block;">
-                <input id="rec" type="text" name="receiver" value="<?php echo $receiver; ?>" hidden>
-                <input id="sen" type="text" name="sender" value="<?php echo $_SESSION['user']; ?>" hidden>
-                <span name="sendMessage" onclick="sendText(); return false;" style="width: 20%; display: inline-block; background-color: transparent; border: none;"><i class="fas fa-paper-plane" style="display: inline;"></i></span>
-            </form>
-        </div>
+    
     </div>
 
-</div>
+</body>
