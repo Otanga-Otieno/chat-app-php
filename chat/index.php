b2651d0763c38da40f947340d48fa5dc108a1931
Otanga-Otieno <otieno.otanga@gmail.com>
2021-11-09 04:40:52 +0300

    Style chat box

--- chat/index.php
+++ chat/index.php
@@ -13,7 +13,7 @@ $sender = $_SESSION['user'];
 <div class="container">
     
     <div class="m-5 rounded" style="border: white solid 1px;">
-        <div style="background-color: #77d7c8; text-align: center; width: 100%;">
+        <div style="background-color: #77d7c8; width: 100%;">
             <span><h3 style="color: white;"><?php echo $receiver; ?></h3></span>
         </div>
         <div style="background-color: white; height: 70vh; width: 100%;">
