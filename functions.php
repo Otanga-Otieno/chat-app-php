c97f18696d722741550de0acb9a8292f4b072984
Otanga-Otieno <otieno.otanga@gmail.com>
2021-11-09 07:45:05 +0300

    Test chat id function

--- functions.php
+++ functions.php
@@ -155,3 +155,15 @@ function get_chats($user1, $user2) {
     }
 
 }
+
+function get_latest_chat_id($sender, $receiver) {
+
+    global $conn;
+    $limit = 1;
+    $stmt = $conn->prepare("SELECT id FROM chat WHERE sender = ? AND receiver = ? ORDER BY id LIMIT ?");
+    $stmt->bind_param("ssi", $sender, $receiver, $limit);
+    $stmt->execute();
+    $stmt->bind_result($result);
+    return $result;
+
+}
--- test.php
+++ test.php
@@ -3,9 +3,6 @@
 require "functions.php";
 require "header.htm";
 
-?>
+echo get_latest_chat_id("alvinotanga@gmail.com", "jvn@mit");
 
-<script>
-    livereceiver("<?php echo $_SESSION['user']; ?>");
-</script>
-<button class="btn">PRESS</button>
+?>
