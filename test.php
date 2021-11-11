65f2843555492f3fdf5fba140b49eb40e35663df
Otanga-Otieno <otieno.otanga@gmail.com>
2021-11-11 17:32:03 +0300

    Test get chat function

--- test.php
+++ test.php
@@ -6,6 +6,4 @@ require "header.htm";
 $sender = "alvin@otanga.co.ke";
 $receiver = "jvn@mit";
 
-?>
-
-<button class="btn" onclick='livereceiver("<?php echo $sender; ?>", "<?php echo $receiver; ?>");'>PRESS</button>
+echo get_chat(32);
