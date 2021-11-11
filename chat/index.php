a751cfcb4469c13352d05728aa98687b0b5cb968
Otanga-Otieno <otieno.otanga@gmail.com>
2021-11-11 13:48:51 +0300

    Call function

--- chat/index.php
+++ chat/index.php
@@ -11,7 +11,7 @@ $sender = $_SESSION['user'];
 
 <script ></script>
 
-<body>
+<body onload="livereceiving('<?php echo $sender; ?>', '<?php echo $receiver; ?>');">
     
     <div class="container">
     
