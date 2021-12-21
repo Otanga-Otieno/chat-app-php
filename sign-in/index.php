2a0d4abc388286a0705b1093f82b9997aa3cd3f5
Otanga-Otieno <otieno.otanga@gmail.com>
2021-12-21 14:18:27 +0300

    Style page

--- sign-in/index.php
+++ sign-in/index.php
@@ -11,19 +11,19 @@ require "../header.htm";
         
         <label for="">
             <span>Email: </span><br>
-            <input type="email" name="uemail">
+            <input type="email" name="uemail" required>
         </label>
         <br>
 
         <label for="">
             <span>Password: </span><br>
-            <input type="password" name="upassword">
+            <input type="password" name="upassword" required>
         </label>
         <br>
 
-        <button class="m-2 btn" type="submit" style="background-color: #77d7c8;" name="signin">Sign In</button>
+        <button class="m-2 btn" type="submit" style="background-color: #77d7c8;" name="signin">Sign In</button><br>
 
-        <a class="btn btn-outline-dark" href="'.$client->createAuthUrl().'" role="button" style="text-transform:none">
+        <a class="m-1 btn btn-outline-dark" href="'.$client->createAuthUrl().'" role="button" style="text-transform:none">
             <img width="20px" style="margin-bottom:3px; margin-right:5px" alt="Google sign-in" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" />Login with Google
         </a>
     </form>
