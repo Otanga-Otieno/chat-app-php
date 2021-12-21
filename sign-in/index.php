9763d0ec772cf84cfa6ec1a1cb51a29a78c10275
Otanga-Otieno <otieno.otanga@gmail.com>
2021-12-21 14:13:02 +0300

    Add log in with Google button

--- sign-in/index.php
+++ sign-in/index.php
@@ -22,5 +22,9 @@ require "../header.htm";
         <br>
 
         <button class="m-2 btn" type="submit" style="background-color: #77d7c8;" name="signin">Sign In</button>
+
+        <a class="btn btn-outline-dark" href="'.$client->createAuthUrl().'" role="button" style="text-transform:none">
+            <img width="20px" style="margin-bottom:3px; margin-right:5px" alt="Google sign-in" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" />Login with Google
+        </a>
     </form>
 </div>
