cfa1b966a800e16c5e05ea52ff6b853c840b3ef9
Otanga-Otieno <otieno.otanga@gmail.com>
2021-12-27 11:08:06 +0300

    Add PHPMailer library

--- functions.php
+++ functions.php
@@ -1,5 +1,12 @@
 <?php
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/SMTP.php';
+
 require "config.php";
 
 session_start();
--- gauth.php
+++ gauth.php
@@ -13,27 +13,27 @@ $client->setRedirectUri($redirectUri);
 
 if (isset($_GET['code'])) {
 
-    $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
-    if(isset($token['access_token'])) {
-      $client->setAccessToken($token['access_token']);
-    } else {
-      header("Location: sign-in/");
-    }
-   
-    $google_oauth = new Google_Service_Oauth2($client);
-    $google_account_info = $google_oauth->userinfo->get();
-    $email =  $google_account_info->email;
-    $name =  $google_account_info->name;
-  
-    if(check_google_user($email)) {
-      login($email);
-      header("Location: ./");
-    } else {
-      create_google_user($email);
-      login($email);
-      header("Location: ./");
-    }
-  
-} else {
+  $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+  if(isset($token['access_token'])) {
+    $client->setAccessToken($token['access_token']);
+  } else {
     header("Location: sign-in/");
+  }
+ 
+  $google_oauth = new Google_Service_Oauth2($client);
+  $google_account_info = $google_oauth->userinfo->get();
+  $email =  $google_account_info->email;
+  $name =  $google_account_info->name;
+
+  if(check_google_user($email)) {
+    login($email);
+    header("Location: ./");
+  } else {
+    create_google_user($email);
+    login($email);
+    header("Location: ./");
+  }  
+
+} else {
+  header("Location: sign-in/");
 }
