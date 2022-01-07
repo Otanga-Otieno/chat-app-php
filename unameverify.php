332d5d3a67c38279eb60f68e4c11b9d0149a1431
Otanga-Otieno <otieno.otanga@gmail.com>
2022-01-07 23:03:01 +0300

    Json encode API response

--- unameverify.php
+++ unameverify.php
@@ -8,9 +8,9 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
     $bool = check_username($name);
     
     if($bool) {
-        echo 1;
+        echo json_encode(1);
     } else {
-        echo 0;
+        echo json_encode(0);
     }
 
 }
