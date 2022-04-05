38fbe214d35c89124085fca096183363c9f2e45e
Otanga-Otieno <otieno.otanga@gmail.com>
2022-04-05 19:05:28 +0300

    Style user list

--- index.php
+++ index.php
@@ -17,12 +17,16 @@ $all = all_users($user);
             <input class="rounded" style="width: 100%;" type="text" name="search" placeholder="Search global users" oninput="searchUser(this.value)" autocomplete="off">
         </form>
     
-        <ul class="p-2" id="userList" style="list-style-type: none; padding: 0; background-color: black; color: white; width: 100%;">
+        <div class="p-2" id="userList" style="list-style-type: none; padding: 0; width: 100%;">
             <li></li><hr>
         <?php foreach($all as $email) { ?>
-            <li><a href="chat/?to=<?php echo $email; ?>"><?php echo $email; ?></a></li><hr>
+            <div class="card py-3 m-1" style="text-align: center; width: 100%;">
+                <span style="color: black;"><b><?php echo $email; ?></b></span><br>
+                <div style="width: 100%; text-align: center;"><a class="btn mainbg white" href="chat/?to=<?php echo $email; ?>">Message</a></div>
+                
+            </div>
         <?php } ?>
-        </ul>
+        </div>
     </div>
 
 </div>
