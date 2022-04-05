2fd4fba6c5900de366c80893d91199e022205550
Otanga-Otieno <otieno.otanga@gmail.com>
2022-04-05 15:13:00 +0300

    Fix class name references

--- sign-up/index.php
+++ sign-up/index.php
@@ -28,6 +28,6 @@ require "../header.htm";
         </label>
         <br>
 
-        <button class="m-2 btn main_color" id="signupBtn" type="submit" name="signup">Sign Up</button>
+        <button class="m-2 btn mainbg" id="signupBtn" type="submit" name="signup">Sign Up</button>
     </form>
 </div>
