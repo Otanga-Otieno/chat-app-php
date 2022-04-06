eb98f2e637154643abbb6f4b1d975662ce893fc4
Otanga-Otieno <otieno.otanga@gmail.com>
2022-04-06 14:08:03 +0300

    Style chat page

--- chat/index.php
+++ chat/index.php
@@ -18,19 +18,19 @@ if($receiver == $sender) header("Location: ../");
     <div class="container">
     
         <div class="m-2 m-md-5 rounded" style="border: white solid 1px;">
-            <div style="background-color: #77d7c8; text-align: center; width: 100%;">
+            <div class="mainbg" style="text-align: center; width: 100%;">
                 <span><h3 style="color: white;"><?php echo $receiver_username; ?></h3></span>
             </div>
             <div class="p-2" id="chatbox" style="background-color: white; height: 70vh; width: 100%; overflow-y: scroll;">
                 <?php get_chats($sender, $receiver); ?>
                 <span id="lcid" hidden><?php echo get_latest_chat_id($receiver, $sender); ?></span>
             </div>
-            <div style="background-color: #77d7c8; width: 100%;">
+            <div class="mainbg" style="width: 100%;">
             <form class="p-1" action="">
                     <input class="rounded" id="msg" type="text" placeholder="Type a message" style="width: 85%; color: black; display: inline-block; border: none;">
                     <input id="rec" type="text" name="receiver" value="<?php echo $receiver_username; ?>" hidden>
                     <input id="sen" type="text" name="sender" value="<?php echo $_SESSION['user']; ?>" hidden>
-                    <div style="display: inline-block; text-align: center; width: 10%;"><span name="sendMessage" onclick="sendText(); return false;" style="display: inline-block; background-color: transparent; border: none;"><i class="fas fa-paper-plane" style="display: inline; cursor: pointer;"></i></span></div>
+                    <div style="display: inline-block; text-align: center; width: 10%;"><span name="sendMessage" onclick="sendText(); return false;" style="display: inline-block; background-color: transparent; border: none;"><i class="fas fa-paper-plane" style="display: inline; cursor: pointer; color: white;"></i></span></div>
             </form>
             </div>
         </div>
